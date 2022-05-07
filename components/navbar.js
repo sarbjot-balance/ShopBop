b1456a05c85af9e7ea9fffa2f57c1546528977b2
sarb908 <sarbjots908@gmail.com>
2022-05-07 21:19:24 +0530

    final code

---
+++ components/navbar.js
@@ -0,0 +1,617 @@
+function navbar() {
+  return `  <div class ="navis"> <div class="containers-1">
+    <div id="lang">
+      <div id="country">
+        <h3>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="iconed"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+            />
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+            />
+          </svg>
+          IN<svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="iconed"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M19 9l-7 7-7-7"
+            />
+          </svg>
+        </h3>
+        <div id="countryList">
+          <h3>India(IN)</h3>
+          <h3>United States (US)</h3>
+          <h3>Canada (CA)</h3>
+          <h3>China (CN)</h3>
+        </div>
+      </div>
+
+      <div id="en">
+        <h3>
+          EN<svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="iconed"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M19 9l-7 7-7-7"
+            />
+          </svg>
+        </h3>
+        <div id="language">
+          <h3>English</h3>
+          <h3>Chinese (中文)</h3>
+          <h3>French (Français)</h3>
+          <h3>German (Deutsch))</h3>
+        </div>
+      </div>
+      <div id="currencies">
+        <h3>
+          USD<svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="iconed"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M19 9l-7 7-7-7"
+            />
+          </svg>
+        </h3>
+        <div id="currenciesChild">
+          <div>
+            <h2>POPULAR CURRENCIES</h2>
+            <h3>US Dollar ($USD)</h3>
+            <h3>Chinese Yuan Renminbi (¥CNY)</h3>
+            <h3>Euro (€EUR)</h3>
+            <h3>Canadian Dollar ($CAD)</h3>
+            <h3>Pound Sterling (£GBP)</h3>
+            <h3>Japanese Yen (¥JPY)</h3>
+            <h3>South Korean Won (₩KRW)</h3>
+            <h3>Russian Ruble (RUB)</h3>
+            <h3>Emirati Dirham (AED)</h3>
+          </div>
+          <div>
+            <h2>MORE OPTIONS</h2>
+            <h3>Australian Dollar ($AUD)</h3>
+            <h3>Brazilian Real ($BRL)</h3>
+            <h3>Danish Krone (DKK)</h3>
+            <h3>Hong Kong Dollar ($HKD)</h3>
+            <h3>Indian Rupee (₹INR)</h3>
+            <h3>Israeli Shekel (₪ILS)</h3>
+            <h3>Kuwaiti Dinar (KWD)</h3>
+            <h3>Mexican Peso ($MXN)</h3>
+            <h3>Polish Zloty (PLN)</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div>
+      <h3>FREE express international delivery + EASY returns</h3>
+    </div>
+    <div id="sign">
+      <div id="signChild">
+        <h3>
+          Sign In / Register
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="iconed"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M19 9l-7 7-7-7"
+            />
+          </svg>
+        </h3>
+
+        <div id="signChilds">
+          <h3>Account</h3>
+          <h3>Orders</h3>
+          <h3>My Loyalty</h3>
+          <h3>My Reviews</h3>
+          <h3>My Hearts</h3>
+          <h3>My Wish List</h3>
+          <h3>My Designers</h3>
+        </div>
+      </div>
+      <div id="wishlisteds">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="iconed wishlist"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      </div>
+      <div id="carteds">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="iconed"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
+          />
+        </svg>
+      </div>
+      <div id ="totalItemsCart">0</div>
+    </div>
+  </div>
+  <div id="logo">
+    <img
+      src="https://m.media-amazon.com/images/G/01/Shopbop/p/pcs/shopbop/media/3/images/logos/rebrand_shopbop_logo_2x_1-3.png"
+      alt=""
+    />
+  </div>
+  <div id="containers-2">
+    <div>
+      <h3>WHAT'S NEW</h3>
+      <div class="new">
+        <div>
+          <h3>WHAT'S NEW</h3>
+
+          <p>New Today</p>
+          <p>New This Week</p>
+          <p>Top Sellers</p>
+          <p>Designer Boutique</p>
+          <p>All What's New</p>
+        </div>
+        <div>
+          <h3>WHAT'S NEW BY CATEGORY</h3>
+          <p>Clothing</p>
+          <p>Shoes</p>
+          <p>Bags</p>
+          <p>Jewelry & Accessories</p>
+          <p>Men</p>
+        </div>
+        <div>
+          <h3>DISCOVER</h3>
+          <p>Party Perfect</p>
+          <p>Destination Dresses</p>
+          <p>The Latest From Cult Gaia</p>
+          <p>Adventure Time</p>
+          <p>Dream Dresses</p>
+        </div>
+        <div>
+          <img
+            src="https://m.media-amazon.com/images/G/01/Shopbop/media/desktop/topnav/2022/2022_04/sb_20220425_desktop_whatsnew_slot1_dresses.jpg"
+            alt=""
+          />
+          <p>The Dress Shop</p>
+          <p><a href="">Shop Now</a></p>
+        </div>
+      </div>
+    </div>
+    <div>
+      <h3>MEN</h3>
+      <div class="new">
+        <div>
+          <h3>MEN</h3>
+
+          <p>Clothing</p>
+          <p>Bags</p>
+
+          <p>Accessories</p>
+          <p>All What's New</p>
+        </div>
+        <div>
+          <h3>FEATURED DESIGNERS</h3>
+          <p>AMI</p>
+          <p>Isabel Marant</p>
+          <p>Maison Kitsune</p>
+          <p>Polo Ralph Lauren</p>
+          <p>Veja</p>
+          <p>All Men's Designers</p>
+        </div>
+        <div>
+          <h3>DISCOVER</h3>
+          <p>What's New</p>
+          <p>Destination Dresses</p>
+          <p>Top Sellers</p>
+          <p>The Basics</p>
+          <p>Explore the Latest</p>
+        </div>
+        <div>
+          <img
+            src="https://m.media-amazon.com/images/G/01/Shopbop/media/desktop/topnav/2022/2022_04/sb_20220425_desktop_men_slot1_freedommoses.jpg"
+            alt=""
+          />
+          <p>Slide Into: Freedom Moses</p>
+          <p><a href="">Shop Now</a></p>
+        </div>
+      </div>
+    </div>
+    <div>
+      <h3>OUR FAVORITES</h3>
+
+      <div class="new">
+        <div>
+          <h3>OUR FAVORITES</h3>
+
+          <p>Spring '22 Trend Edit</p>
+          <p>AAPI Designers & AAPI-Owned Brands</p>
+
+          <p>Black Designers & Black-Owned Brands</p>
+          <p>Back In Stock</p>
+          <p>Caroline's Hearts</p>
+          <p>Designer Boutique</p>
+          <p>Editors' Picks</p>
+          <p>Fashion Finds: Under $200</p>
+        </div>
+        <div>
+          <h3>FEATURED DESIGNERS</h3>
+          <p>AMI</p>
+          <p>Isabel Marant</p>
+          <p>Maison Kitsune</p>
+          <p>Polo Ralph Lauren</p>
+          <p>Veja</p>
+          <p>All Men's Designers</p>
+        </div>
+        <div>
+          <h3>DISCOVER</h3>
+          <p>What's New</p>
+          <p>Destination Dresses</p>
+          <p>Top Sellers</p>
+          <p>The Basics</p>
+          <p>Explore the Latest</p>
+        </div>
+        <div>
+          <img
+            src="https://m.media-amazon.com/images/G/01/Shopbop/media/desktop/topnav/2022/2022_04/sb_20220425_desktop_ourfavorites_slot1_topsellers.jpg"
+            alt=""
+          />
+          <p>Top Sellers</p>
+          <p><a href="">Shop Now</a></p>
+        </div>
+        <div>
+          <img
+            src="https://m.media-amazon.com/images/G/01/Shopbop/media/desktop/topnav/2022/2022_04/sb_20220425_desktop_ourfavorites_slot3_vacation.jpg"
+            alt=""
+          />
+          <p>Vacation Wardrobe</p>
+          <p><a href="">Shop Now</a></p>
+        </div>
+      </div>
+    </div>
+    <div>
+      <h3>DESIGNERS</h3>
+      <div class="new">
+        <div>
+          <h3>WHAT'S NEW</h3>
+
+          <p>New Today</p>
+          <p>New This Week</p>
+          <p>Top Sellers</p>
+          <p>Designer Boutique</p>
+          <p>All What's New</p>
+        </div>
+        <div>
+          <h3>WHAT'S NEW BY CATEGORY</h3>
+          <p>Clothing</p>
+          <p>Shoes</p>
+          <p>Bags</p>
+          <p>Jewelry & Accessories</p>
+          <p>Men</p>
+        </div>
+        <div>
+          <h3>DISCOVER</h3>
+          <p>Party Perfect</p>
+          <p>Destination Dresses</p>
+          <p>The Latest From Cult Gaia</p>
+          <p>Adventure Time</p>
+          <p>Dream Dresses</p>
+        </div>
+        <div>
+          <img
+            src="https://m.media-amazon.com/images/G/01/Shopbop/media/desktop/topnav/2022/2022_04/sb_20220425_desktop_whatsnew_slot1_dresses.jpg"
+            alt=""
+          />
+          <p>The Dress Shop</p>
+          <p><a href="">Shop Now</a></p>
+        </div>
+      </div>
+    </div>
+    <div>
+      <h3>CLOTHING</h3>
+      <div class="new">
+        <div>
+          <h3>WOMEN</h3>
+
+          <p>Clothing</p>
+          <p>Bags</p>
+
+          <p>Accessories</p>
+          <p>All What's New</p>
+        </div>
+        <div>
+          <h3>FEATURED DESIGNERS</h3>
+          <p>AMI</p>
+          <p>Isabel Marant</p>
+          <p>Maison Kitsune</p>
+          <p>Polo Ralph Lauren</p>
+          <p>Veja</p>
+          <p>All Men's Designers</p>
+        </div>
+        <div>
+          <h3>DISCOVER</h3>
+          <p>What's New</p>
+          <p>Destination Dresses</p>
+          <p>Top Sellers</p>
+          <p>The Basics</p>
+          <p>Explore the Latest</p>
+        </div>
+        <div>
+          <img
+            src="https://m.media-amazon.com/images/G/01/Shopbop/media/desktop/topnav/2022/2022_05/sb_20220502_desktop_clothing_slot1_EnglishFactory.jpg"
+            alt=""
+          />
+          <p>New From English Factory</p>
+          <p><a href="">Shop Now</a></p>
+        </div>
+      </div>
+    </div>
+    <div>
+      <h3>ACTIVE</h3>
+      <div class="new">
+        <div>
+          <h3>OUR FAVORITES</h3>
+
+          <p>Spring '22 Trend Edit</p>
+          <p>AAPI Designers & AAPI-Owned Brands</p>
+
+          <p>Black Designers & Black-Owned Brands</p>
+          <p>Back In Stock</p>
+          <p>Caroline's Hearts</p>
+          <p>Designer Boutique</p>
+          <p>Editors' Picks</p>
+          <p>Fashion Finds: Under $200</p>
+        </div>
+        <div>
+          <h3>FEATURED DESIGNERS</h3>
+          <p>AMI</p>
+          <p>Isabel Marant</p>
+          <p>Maison Kitsune</p>
+          <p>Polo Ralph Lauren</p>
+          <p>Veja</p>
+          <p>All Men's Designers</p>
+        </div>
+        <div>
+          <h3>DISCOVER</h3>
+          <p>What's New</p>
+          <p>Destination Dresses</p>
+          <p>Top Sellers</p>
+          <p>The Basics</p>
+          <p>Explore the Latest</p>
+        </div>
+        <div>
+          <img
+            src="https://m.media-amazon.com/images/G/01/Shopbop/media/desktop/topnav/2022/2022_04/sb_20220425_desktop_ourfavorites_slot1_topsellers.jpg"
+            alt=""
+          />
+          <p>Top Sellers</p>
+          <p><a href="">Shop Now</a></p>
+        </div>
+        <div>
+          <img
+            src="https://m.media-amazon.com/images/G/01/Shopbop/media/desktop/topnav/2022/2022_04/sb_20220425_desktop_ourfavorites_slot3_vacation.jpg"
+            alt=""
+          />
+          <p>Vacation Wardrobe</p>
+          <p><a href="">Shop Now</a></p>
+        </div>
+      </div>
+    </div>
+    <div>
+      <h3>SHOES</h3>
+      <div class="new">
+        <div>
+          <h3>WHAT'S NEW</h3>
+
+          <p>New Today</p>
+          <p>New This Week</p>
+          <p>Top Sellers</p>
+          <p>Designer Boutique</p>
+          <p>All What's New</p>
+        </div>
+        <div>
+          <h3>WHAT'S NEW BY CATEGORY</h3>
+          <p>Clothing</p>
+          <p>Shoes</p>
+          <p>Bags</p>
+          <p>Jewelry & Accessories</p>
+          <p>Men</p>
+        </div>
+        <div>
+          <h3>DISCOVER</h3>
+          <p>Party Perfect</p>
+          <p>Destination Dresses</p>
+          <p>The Latest From Cult Gaia</p>
+          <p>Adventure Time</p>
+          <p>Dream Dresses</p>
+        </div>
+        <div>
+          <img
+            src="https://m.media-amazon.com/images/G/01/Shopbop/media/desktop/topnav/2022/2022_04/sb_20220425_desktop_whatsnew_slot1_dresses.jpg"
+            alt=""
+          />
+          <p>The Dress Shop</p>
+          <p><a href="">Shop Now</a></p>
+        </div>
+      </div>
+    </div>
+    <div>
+      <h3>BAGS</h3>
+      <div class="new">
+        <div>
+          <h3>MEN</h3>
+
+          <p>Clothing</p>
+          <p>Bags</p>
+
+          <p>Accessories</p>
+          <p>All What's New</p>
+        </div>
+        <div>
+          <h3>FEATURED DESIGNERS</h3>
+          <p>AMI</p>
+          <p>Isabel Marant</p>
+          <p>Maison Kitsune</p>
+          <p>Polo Ralph Lauren</p>
+          <p>Veja</p>
+          <p>All Men's Designers</p>
+        </div>
+        <div>
+          <h3>DISCOVER</h3>
+          <p>What's New</p>
+          <p>Destination Dresses</p>
+          <p>Top Sellers</p>
+          <p>The Basics</p>
+          <p>Explore the Latest</p>
+        </div>
+        <div>
+          <img
+            src="https://m.media-amazon.com/images/G/01/Shopbop/media/desktop/topnav/2022/2022_04/sb_20220425_desktop_men_slot1_freedommoses.jpg"
+            alt=""
+          />
+          <p>Slide Into: Freedom Moses</p>
+          <p><a href="">Shop Now</a></p>
+        </div>
+      </div>
+    </div>
+    <div>
+      <h3>ACCESSORIES</h3>
+      <div class="new">
+        <div>
+          <h3>MEN</h3>
+
+          <p>Clothing</p>
+          <p>Bags</p>
+
+          <p>Accessories</p>
+          <p>All What's New</p>
+        </div>
+        <div>
+          <h3>FEATURED DESIGNERS</h3>
+          <p>AMI</p>
+          <p>Isabel Marant</p>
+          <p>Maison Kitsune</p>
+          <p>Polo Ralph Lauren</p>
+          <p>Veja</p>
+          <p>All Men's Designers</p>
+        </div>
+        <div>
+          <h3>DISCOVER</h3>
+          <p>What's New</p>
+          <p>Destination Dresses</p>
+          <p>Top Sellers</p>
+          <p>The Basics</p>
+          <p>Explore the Latest</p>
+        </div>
+        <div>
+          <img
+            src="https://m.media-amazon.com/images/G/01/Shopbop/media/desktop/topnav/2022/2022_05/sb_20220502_desktop_accessories_slot1_HatAttack.jpg"
+            alt=""
+          />
+
+          <p>Top It Off: Hat Attack</p>
+          <p><a href="">Shop Now</a></p>
+        </div>
+      </div>
+    </div>
+    <div>
+      <h3>SALE</h3>
+      <div class="new">
+        <div>
+          <h3>OUR FAVORITES</h3>
+
+          <p>Spring '22 Trend Edit</p>
+          <p>AAPI Designers & AAPI-Owned Brands</p>
+
+          <p>Black Designers & Black-Owned Brands</p>
+          <p>Back In Stock</p>
+          <p>Caroline's Hearts</p>
+          <p>Designer Boutique</p>
+          <p>Editors' Picks</p>
+          <p>Fashion Finds: Under $200</p>
+        </div>
+        <div>
+          <h3>FEATURED DESIGNERS</h3>
+          <p>AMI</p>
+          <p>Isabel Marant</p>
+          <p>Maison Kitsune</p>
+          <p>Polo Ralph Lauren</p>
+          <p>Veja</p>
+          <p>All Men's Designers</p>
+        </div>
+        <div>
+          <h3>DISCOVER</h3>
+          <p>What's New</p>
+          <p>Destination Dresses</p>
+          <p>Top Sellers</p>
+          <p>The Basics</p>
+          <p>Explore the Latest</p>
+        </div>
+        <div>
+          <img
+            src="https://m.media-amazon.com/images/G/01/Shopbop/media/desktop/topnav/2022/2022_04/sb_20220425_desktop_ourfavorites_slot1_topsellers.jpg"
+            alt=""
+          />
+          <p>Top Sellers</p>
+          <p><a href="">Shop Now</a></p>
+        </div>
+        <div>
+          <img
+            src="https://m.media-amazon.com/images/G/01/Shopbop/media/desktop/topnav/2022/2022_03/sb_20220131_desktop_sale_slot1_newtosale.jpg"
+            alt=""
+          />
+          <p>New to Sale</p>
+          <p><a href="">Shop Now</a></p>
+        </div>
+      </div>
+    </div>
+  </div> </div>`;
+}
+export default navbar;
